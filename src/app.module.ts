b4bb7d5740b57107422ddc5a9c2168942c65df05
nawelfh <nawel.felah94@gmail.com>
2022-03-29 16:32:14 +0100

    Configurer env MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'; 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [
     ProductModule, 
-    MongooseModule.forRoot(
-      'mongodb+srv://nawel:afTMRYnpzTCN1hXA@cluster0.p21r4.mongodb.net/nestjs-productApp?retryWrites=true&w=majority'
-      ),
+      ConfigModule.forRoot({ envFilePath: ['.env'] }),
+      MongooseModule.forRoot(process.env.MONGO_INFO),
     ],
   controllers: [AppController],
   providers: [AppService],
